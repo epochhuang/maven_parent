5dc43b244844e8d4a5be1a4184ffb51c03d5d411
epochhuang <huangxinbelief@gmail.com>
2020-09-06 02:20:29 +0800

    第一次commit

---
+++ maven_module02/src/main/java/Test1.java
@@ -0,0 +1,11 @@
+/**
+ * @author huangxin
+ * @description
+ * @date 2020/8/18
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        new Person();
+    }
+}
---
+++ maven_module03/src/main/java/Person.java
@@ -0,0 +1,7 @@
+/**
+ * @author huangxin
+ * @description
+ * @date 2020/8/18
+ */
+public class Person {
+}
